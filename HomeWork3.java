9eacb03b6d106c133b56363652d8177d8433d82c
Kpany3 <51132753+Kpany3@users.noreply.github.com>
2020-05-24 21:33:19 +0300

    Update HomeWork3
    Добавил комментарии

--- HomeWork3.java
+++ HomeWork3.java
@@ -9,22 +9,22 @@ public class HomeWork3 {
     public static void main(String[] args) {
         int randomNum = (int) (Math.random() * 10);// генерируем рандомнео число
         System.out.println("Игра. Угадайте загаданное число от 0 до 9."); //выводим заголовок начала игры
-        String getNum = guessnumber(randomNum);//
-        System.out.println(getNum);
-        System.out.println("Было загадано число " + randomNum);
+        String getNum = guessnumber(randomNum);// метод угадывания рандомного числа
+        System.out.println(getNum);// вывод результата
+        System.out.println("Было загадано число " + randomNum);// вывод результата
     }
 
     public static String guessnumber(int randomNum) {
-        int x;
-        int tryNum = 1;
+        int x;//вводимая переменная
+        int tryNum = 1;// первая попытка
         do {
-            x = sc.nextInt();
-            if (tryNum < 4) {
-                if (x > randomNum) {
+            x = sc.nextInt();// консоль просит ввести число
+            if (tryNum < 4) {//условие проверки колиечства попыток
+                if (x > randomNum) {//если введеное число больше
                     System.out.println("Загаданное число меньше");
-                } else if (x < randomNum){
+                } else if (x < randomNum){//если введеное число меньше
                     System.out.println("Загаданное число больше");
-                } else{
+                } else{//если введеное число угадано
                     System.out.println("Число угадано за "+ tryNum + " попытки");
                     return "Вы выграли";
                 }
